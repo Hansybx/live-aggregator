b3ee0518b6b48a66dae312fb8be9d5598d3e040f
Hansybx <hansybx@outlook.com>
2023-03-25 21:13:53 +0800

    feat init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/App'
-import './index.css'
+import routers from './router';
+import {
+  RouterProvider,
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routers} />
   </React.StrictMode>,
 )
---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/home"
+
+
+const routers = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+]);
+
+export default routers;
