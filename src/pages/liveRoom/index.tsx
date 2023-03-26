ebdb8a8fbecb3a7025b8fbc5267e4a58b4f0814d
Hansybx <hansybx@outlook.com>
2023-03-26 17:33:45 +0800

    feat 新增直播源prop

--- src/pages/liveRoom/index.tsx
+++ src/pages/liveRoom/index.tsx
@@ -2,6 +2,6 @@ import LivePlayer from './livePlayer'
 
 export default function LiveRoom() {
     return (
-        <><div>liveRoom</div><LivePlayer /></>
+        <><div>liveRoom</div><LivePlayer liveUrl={"http://hw-tct.douyucdn.cn/live/288016rlols5.flv?uuid="} /></>
     )
 }
--- src/pages/liveRoom/livePlayer.tsx
+++ src/pages/liveRoom/livePlayer.tsx
@@ -2,31 +2,34 @@ import FlvJs from 'flv.js'
 import DPlayer from 'dplayer';
 import { useEffect } from 'react';
 
-export default function livePlayer() {
-    useEffect(() => {
-        const options = {
-          container: document.getElementById('dplayer'),
-          live: true,
-          autoplay: true,
-          video: {
-            url: 'http://hw-tct.douyucdn.cn/live/288016rlols5.flv?uuid=',
-            type: 'customFlv',
-            customType: {
-              customFlv: function (video: any) {
-                const flvPlayer = FlvJs.createPlayer({
-                  type: 'flv',
-                  url: video.src,
-                });
-                flvPlayer.attachMediaElement(video);
-                flvPlayer.load();
-              },
-            },
+interface IProps {
+  liveUrl: string
+}
+
+export default function livePlayer(props: IProps) {
+  useEffect(() => {
+    const options = {
+      container: document.getElementById('dplayer'),
+      live: true,
+      autoplay: true,
+      video: {
+        url: props.liveUrl,
+        type: 'customFlv',
+        customType: {
+          customFlv: function (video: any) {
+            const flvPlayer = FlvJs.createPlayer({
+              type: 'flv',
+              url: video.src,
+            });
+            flvPlayer.attachMediaElement(video);
+            flvPlayer.load();
           },
-        };
-        const dp = new DPlayer(options);
-        console.log('test', options, dp)
-        dp.play();
-      })
+        },
+      },
+    };
+    const dp = new DPlayer(options);
+    dp.play();
+  })
   return (
     <div id="dplayer"></div>
   )
